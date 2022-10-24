7b69573825fe438ffd26681cc6beb1db037488f6
ALFA <alfa.sunny.22@gmail.com>
2022-10-25 02:49:18 +0600

    Practice Basic JavaScript Code

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,16 @@
 console.log('Hello, JavaScript World...');
+
+
+/*
+* Basic Math related topices need to know to solve Math related problems.
+
+* 1. number ==>  55, -66;
+* 2. Integer, Odd, Even number;
+* 3. Folat, Decimal;
+* 4. Operation: addition, subtraction, multiplication, division, remainder / moduler;
+* 5. Percent, Average, Max, Min;
+* 6. Comparison:  >,  <,  ==,  !=, >=, <=;
+* ::: EXTRA ::::
+* 7. Prime, Permutation...;
+* 
+*/
---
+++ operation.js
@@ -0,0 +1,38 @@
+var onionPrice = 30;
+var eggPrice = 50;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+
+// console.log( "Total Price is " + (onionPrice + eggPrice) );
+
+var totalPrice = onionPrice + eggPrice;
+// console.log("Price is " + totalPrice);
+
+
+//------------------------ Mathematical Operation  ----------------
+
+// For total : a + b;
+// For difference :  a - b;
+// For multiplication: a * b;
+// For Division : a / b;
+
+
+
+//-----------------------------------------------------------------//
+
+var price1 = 10;
+price1 = price1 + 5;
+// console.log(price1);  //  10 + 5 = 15
+price1 = price1 - 3;
+// console.log(price1);  // 15-3=12
+
+//------------------------------------------------------------//
+
+var num = 4;
+// console.log(num)
+num++;
+// console.log(num)
+
+
+//--------------------------------------------//
